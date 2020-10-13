27d93f0c293dde823115ae2348ee5efd687c04c8
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-13 09:04:30 -0600

    Load dataset

--- Eclat.py
+++ Eclat.py
@@ -7,6 +7,9 @@ import pandas as pd
 
 # Data Preprocessing 
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
 # Training the Eclat model on the dataset
 
